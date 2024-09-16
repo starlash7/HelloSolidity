7153a0728198290891da9d5bb21bfa52a632e4ed
starlash7 <yang92sw@gmail.com>
2024-09-17 00:53:23 +0900

    DefaultValues

---
+++ DefaultValues.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.7;
+
+contract DefaultValues {
+    bool public b; // false
+    uint public u; // 0
+    int public i; // 0
+    address public a; // 0x 40개
+    bytes32 public b32; // 0x 64개
+    
+}
