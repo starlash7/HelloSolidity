6f25488c6d177e4f500a7157b495b5ad01f07e28
starlash7 <yang92sw@gmail.com>
2024-09-18 19:31:36 +0900

    constants

---
+++ Constants.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.7;
+
+contract Constants {
+    address public constant MY_ADDRESS = 0x475268163B3a7549ca3dD6AbFF075040b5cf09E1;
+    uint public constant MY_UINT = 123;
+    //21442 gas
+}
+
+contract Var {
+    address public MY_ADDRESS = 0x475268163B3a7549ca3dD6AbFF075040b5cf09E1;
+    //23553 gas
+}
