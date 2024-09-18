ecded7d359c67dad56cf5c0b1464a839ee1d89ab
starlash7 <yang92sw@gmail.com>
2024-09-18 22:20:21 +0900

    Array

---
+++ Array.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.7;
+
+contract Array {
+    uint[] public nums = [1, 2, 3];
+    uint[3] public numsFiexed = [4, 5, 6];
+
+    function examples() external {
+        nums.push(4); // [1, 2, 3, 4]
+        uint x = nums[1];
+        nums[2] = 777; // [1, 2, 777, 4]
+        delete nums[1]; // [1, 0, 777, 4]
+        nums.pop(); // [1, 0, 777]
+        uint len = nums.length;
+
+        // create array in memory
+        uint[] memory a = new uint[](5);
+        a[1] = 123;
+
+    }
+    function returnArray() external view returns(uint[] memory) {
+        return nums;
+    }
+
+
+}
